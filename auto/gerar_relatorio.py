bc615a03560a4ecfc1f14982d597163ce5b6e518
Italo-Soousa <italomadeira21@gmail.com>
2025-06-09 15:37:48 -0300

    terminando a parte de geração de relatório

--- auto/gerar_relatorio.py
+++ auto/gerar_relatorio.py
@@ -10,39 +10,62 @@ import time
 def gerar_relatorios(navegador: WebDriver, data_inicial, data_final):
     espera = WebDriverWait(navegador, 20)
 
+    visoes = [
+    "COTY",
+    "EXCELLENCE",
+    "JUNO",
+    "LOREAL",
+    "LVMH",
+    "PRESTIGE",
+    "PUIG",
+    "SHISEIDO",
+    "SMART BEAUTY",
+    "TFS",
+    "VIZCAYA",
+    "WEITNAUER"
+    ]
+
     try:
+        for visao in visoes:
+            # Navegando até o iframe que está o relatório
+            iframe = espera.until(EC.presence_of_element_located((By.ID, "main")))
+            navegador.switch_to.frame(iframe)
 
-        # Navegando até o iframe que está o relatório
-        iframe = espera.until(EC.presence_of_element_located((By.ID, "main")))
-        navegador.switch_to.frame(iframe)
+            # Selecionando o relatório que está salvo na visão
+            select_element = espera.until(EC.presence_of_element_located((By.ID, 'Form1_id_visao')))
+            seletor = Select(select_element)
+            seletor.select_by_visible_text(visao)
 
-        # Selecionando o relatório que está salvo na visão
-        select_element = espera.until(EC.presence_of_element_located((By.ID, 'Form1_id_visao')))
+            # Gera o relatório
+            botao_gerarRelatorio = espera.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Gerar Relatório"]')))
+            botao_gerarRelatorio.click()
 
-        # Converte para objeto Select
-        seletor = Select(select_element)
+            # Preenchendo o campo das datas 
+            for campo, valor in [("f_data1", data_inicial), ("f_data2", data_final)]:
+                campo_data = espera.until(EC.element_to_be_clickable((By.ID, campo)))
+                campo_data.click()
+                campo_data.send_keys(Keys.CONTROL + "a")
+                campo_data.send_keys(Keys.BACKSPACE)
+                campo_data.send_keys(valor)
 
-        # Seleciona pelo texto visível
-        seletor.select_by_visible_text("LOREAL")
+            botaoOK = espera.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "ui-button-text-only")]')))
+            botaoOK.click()
 
-        # Gera o relatório
-        botao_gerarRelatorio = espera.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Gerar Relatório"]')))
-        botao_gerarRelatorio.click()
+            # Sair do iframe e voltar ao conteúdo principal
+            navegador.switch_to.default_content()
 
-        # Preenchendo o campo das datas 
-        for campo, valor in [("f_data1", data_inicial), ("f_data2", data_final)]:
-            campo_data = espera.until(EC.element_to_be_clickable((By.ID, campo)))
-            campo_data.click()
-            campo_data.send_keys(Keys.CONTROL + "a")
-            campo_data.send_keys(Keys.BACKSPACE)
-            campo_data.send_keys(valor)
+            # Entra no iframe para acessar o relatório
+            iframe = espera.until(EC.presence_of_element_located((By.ID, "main")))
+            navegador.switch_to.frame(iframe)
 
-        botaoOK = espera.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "ui-button-text-only")]')))
-        botaoOK.click()
+            # Clica no botão para gerar o relatório
+            botaoExcel = espera.until(EC.element_to_be_clickable((By.ID, 'botaoExportarXLS')))
+            botaoExcel.click()
 
-        
+            navegador.back()
+            time.sleep(7)
 
-        print("📥 Relatório baixado com sucesso!")
+            print(f"📥 Relatório da visão {visao} baixado com sucesso!")
 
     except Exception as e:
         print(f"❌ Erro ao exportar Excel: {e}")
